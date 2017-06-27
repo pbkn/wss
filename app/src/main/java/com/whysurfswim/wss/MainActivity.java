5c69837c72548b0b075028e7e47edbd4bd09e7f4
PBKN <sakura06>
2017-06-27 20:09:51 +0530

    performance commit

--- app/src/main/java/com/whysurfswim/wss/MainActivity.java
+++ app/src/main/java/com/whysurfswim/wss/MainActivity.java
@@ -2,12 +2,56 @@ package com.whysurfswim.wss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    String ShowOrHideWebViewInitialUse = "show";
+    private WebView webview ;
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview =(WebView)findViewById(R.id.webView);
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        webview.setWebViewClient(new CustomWebViewClient());
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://whysurfswim.com/");
+
+    }
+
+    // This allows for a splash screen
+    // (and hide elements once the page loads)
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+
+            // only make it invisible the FIRST time the app is run
+            if (ShowOrHideWebViewInitialUse.equals("show")) {
+                webview.setVisibility(webview.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            ShowOrHideWebViewInitialUse = "hide";
+            spinner.setVisibility(View.GONE);
+
+            view.setVisibility(webview.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
     }
 }
