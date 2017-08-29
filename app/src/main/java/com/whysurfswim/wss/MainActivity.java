c01924cfc1be454dff00c7dda08aff56a10e9b97
pbkn <ravichandran.prabhakaran@gmail.com>
2017-08-29 19:09:13 +0530

    version 4.0 commit

--- app/src/main/java/com/whysurfswim/wss/MainActivity.java
+++ app/src/main/java/com/whysurfswim/wss/MainActivity.java
@@ -2,10 +2,13 @@ package com.whysurfswim.wss;
 
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.support.v7.app.ActionBar;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,6 +16,10 @@ import android.graphics.Bitmap;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.util.List;
 
@@ -22,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private WebView webview;
     private ProgressBar spinner;
     private Toolbar toolbar;
-    private boolean flag,doubleBack;
+    private boolean flag,doubleBack,fabExpanded;
+    private AdView mAdView;
+    private FloatingActionButton fabInfo,fabAbout,fabShare,fabFacebook,fabLinkedin,fabWhatsapp,fabTwitter;
+    private LinearLayoutCompat layoutAbout,layoutShare,layoutLinkedin,layoutWhatsapp,layoutTwitter,layoutFacebook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +49,87 @@ public class MainActivity extends AppCompatActivity {
         spinner = (ProgressBar) findViewById(R.id.progressBar1);
         webview.setWebViewClient(new CustomWebViewClient());
 
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webview.loadUrl("https://whysurfswim.com/");
+        if(isOnline()) {
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+            webview.loadUrl("https://whysurfswim.com/");
+        }else {
+            finish();
+        }
+
+        MobileAds.initialize(this, "ca-app-pub-6059528612565667/8468844477");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        closeSubMenusFab();
+        fabInfo = (FloatingActionButton) this.findViewById(R.id.fabInfo);
+        fabAbout = (FloatingActionButton) this.findViewById(R.id.fabAbout);
+        fabShare = (FloatingActionButton) this.findViewById(R.id.fabShare);
+        fabLinkedin = (FloatingActionButton) this.findViewById(R.id.fabLinkedin);
+        fabTwitter = (FloatingActionButton) this.findViewById(R.id.fabTwitter);
+        fabWhatsapp = (FloatingActionButton) this.findViewById(R.id.fabWhatsapp);
+        fabFacebook = (FloatingActionButton) this.findViewById(R.id.fabFacebook);
+
+        //When main Fab (Settings) is clicked, it expands if not expanded already.
+        //Collapses if main FAB was open already.
+        //This gives FAB (Settings) open/close behavior
+        fabInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (fabExpanded){
+                    closeSubMenusFab();
+                } else {
+                    openSubMenusFab();
+                }
+            }
+        });
+
+        fabAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webview.loadUrl("https://play.google.com/store/apps/details?id=com.whysurfswim.wss&hl=en");
+            }
+        });
+
+        fabShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, webview.getUrl());
+                shareIntent.setType("text/plain");
+                startActivity(shareIntent);
+            }
+        });
+
+        fabWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webview.loadUrl("https://chat.whatsapp.com/invite/1ACYKZvKAcP7GJqhgG0NBk");
+            }
+        });
+
+        fabLinkedin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                webview.loadUrl("https://www.linkedin.com/company-beta/13269149/");
+            }
+        });
+
+        fabTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webview.loadUrl("https://www.twitter.com/@whysurfswim/");
+            }
+        });
+
+        fabFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webview.loadUrl("https://www.facebook.com/whysurfswim/");
+            }
+        });
     }
 
     @Override
@@ -59,12 +146,80 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //closes FAB submenus
+    private void closeSubMenusFab(){
+        fabInfo = (FloatingActionButton) this.findViewById(R.id.fabInfo);
+        layoutAbout = (LinearLayoutCompat) this.findViewById(R.id.layoutFabAbout);
+        layoutShare = (LinearLayoutCompat) this.findViewById(R.id.layoutFabShare);
+        layoutLinkedin = (LinearLayoutCompat) this.findViewById(R.id.layoutFabLinkedin);
+        layoutTwitter = (LinearLayoutCompat) this.findViewById(R.id.layoutFabTwitter);
+        layoutWhatsapp = (LinearLayoutCompat) this.findViewById(R.id.layoutFabWhatsapp);
+        layoutFacebook = (LinearLayoutCompat) this.findViewById(R.id.layoutFabFacebook);
+
+        layoutLinkedin.setVisibility(View.INVISIBLE);
+        layoutWhatsapp.setVisibility(View.INVISIBLE);
+        layoutTwitter.setVisibility(View.INVISIBLE);
+        layoutShare.setVisibility(View.INVISIBLE);
+        layoutAbout.setVisibility(View.INVISIBLE);
+        layoutFacebook.setVisibility(View.INVISIBLE);
+        fabInfo.setImageResource(R.drawable.ic_info_outline_black_24dp);
+        fabExpanded = false;
+    }
+
+    //Opens FAB submenus
+    private void openSubMenusFab(){
+        fabInfo = (FloatingActionButton) this.findViewById(R.id.fabInfo);
+        layoutAbout = (LinearLayoutCompat) this.findViewById(R.id.layoutFabAbout);
+        layoutShare = (LinearLayoutCompat) this.findViewById(R.id.layoutFabShare);
+        layoutLinkedin = (LinearLayoutCompat) this.findViewById(R.id.layoutFabLinkedin);
+        layoutTwitter = (LinearLayoutCompat) this.findViewById(R.id.layoutFabTwitter);
+        layoutWhatsapp = (LinearLayoutCompat) this.findViewById(R.id.layoutFabWhatsapp);
+        layoutFacebook = (LinearLayoutCompat) this.findViewById(R.id.layoutFabFacebook);
+
+        layoutLinkedin.setVisibility(View.VISIBLE);
+        layoutWhatsapp.setVisibility(View.VISIBLE);
+        layoutTwitter.setVisibility(View.VISIBLE);
+        layoutAbout.setVisibility(View.VISIBLE);
+        layoutShare.setVisibility(View.VISIBLE);
+        layoutFacebook.setVisibility(View.VISIBLE);
+        //Change settings icon to 'X' icon
+        fabInfo.setImageResource(R.drawable.ic_close_black_24dp);
+        fabExpanded = true;
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager conMgr = (ConnectivityManager) getApplicationContext().getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
+
+        if(netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()){
+            Toast.makeText(this, "No Internet connection!", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
 
     // This allows for a splash screen
     // (and hide elements once the page loads)
     private class CustomWebViewClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+
+            if(url.startsWith("https://play.google.com")){
+                Intent shareIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                List<ResolveInfo> matches = getPackageManager().queryIntentActivities(shareIntent, 0);
+                for (ResolveInfo info : matches) {
+                    if (info.activityInfo.packageName.toLowerCase().contains("com.android.vending")) {
+                        shareIntent.setPackage(info.activityInfo.packageName);
+                        flag = true;
+                    }
+                }
+                if (flag) {
+                    startActivity(shareIntent);
+                    webview.reload();
+                } else {
+                    webview.loadUrl(url);
+                }
+            }
 
             if(url.startsWith("market://details")){
                 webview.loadUrl("https://www.flipkart.com/?cmpid=fkrt_affiliate_network_ravichand32&referrer=mat_click_id%3Ddbad0e023ca71d5f3aa21bd92cea8081-20170710-189358");
@@ -160,15 +315,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            if(url.equals("https://www.linkedin.com/company-beta/13269149/")){
+                Intent shareIntent;
+                shareIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company-beta/13269149/"));
+                List<ResolveInfo> matches = getPackageManager().queryIntentActivities(shareIntent, 0);
+                for (ResolveInfo info : matches) {
+                    if (info.activityInfo.packageName.toLowerCase().contains("com.linkedin")) {
+                        shareIntent.setPackage(info.activityInfo.packageName);
+                        flag = true;
+                    }
+                }
+                if (flag) {
+                    startActivity(shareIntent);
+                    webview.reload();
+                } else {
+                    webview.loadUrl("https://play.google.com/store/apps/details?id=com.linkedin.android&hl=en");
+                }
+            }
+
             if (url.startsWith("https://platform")) {
                 Intent shareIntent;
-                if(webview.getUrl().equals("https://www.linkedin.com/company-beta/13269149/")){
-                    shareIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company-beta/13269149/"));
-                } else {
                     shareIntent = new Intent(Intent.ACTION_SEND);
                     shareIntent.putExtra(Intent.EXTRA_TEXT, webview.getUrl());
                     shareIntent.setType("text/plain");
-                }
                 List<ResolveInfo> matches = getPackageManager().queryIntentActivities(shareIntent, 0);
                 for (ResolveInfo info : matches) {
                     if (info.activityInfo.packageName.toLowerCase().contains("com.linkedin")) {
